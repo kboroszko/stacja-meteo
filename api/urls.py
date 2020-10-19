6e8eb055e3a434eb7aa8fe9a080dcbfc47c2735a
Kajetan Boroszko <kajetan.boroszko@gmail.com>
2020-10-19 20:23:35 +0200

    added api endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:station_id>/get_data', views.get_data),
+    path('<int:station_id>/post_data', views.post_data),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the api index.")
 
 
 # Create your views here.
+
+def get_data(request, station_id):
+    # use a handler here
+    return HttpResponse(f"stationId:{station_id}\nget data endpoint.")
+
+
+def post_data(request, station_id):
+    # use a handler here
+    return HttpResponse(f"stationId:{station_id}\npost data endpoint.")
